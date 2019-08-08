f6f74199b3d0631f59cee437e81ffbbc9bf3ca9a
Radek Tomiška <radek.tomiska@gmail.com>
2019-08-08 10:12:03 +0200

    test - jenkins git hook

--- src/test/java/eu/bcvsolutions/forest/index/TestApplication.java
+++ src/test/java/eu/bcvsolutions/forest/index/TestApplication.java
@@ -5,14 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
- * Test app
- * 
+ * Test application.
+ *
  * @author Radek Tomiška
  */
 @EnableJpaRepositories
 @SpringBootApplication
 public class TestApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 	}
